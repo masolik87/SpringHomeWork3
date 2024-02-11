3e7b58f021fb4b1cb26de732477ce736f3da994f
dimon <dimon20.07@mail.ru>
2024-02-11 15:13:44 +0300

    .\controllers\

---
+++ controllers/TaskController.java
@@ -0,0 +1,64 @@
+package HomeWork3.HomeWork3.controllers;
+
+import HomeWork3.HomeWork3.domain.User;
+import HomeWork3.HomeWork3.services.DataProcessingService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private DataProcessingService service;
+
+    /**
+     * Базовый метод вывода всех запросов
+     * @return список запросов
+     */
+    @GetMapping
+    public List<String> getAllTasks()
+    {
+        List<String> tasks = new ArrayList<>();
+        tasks.add("sort");
+        tasks.add("filter");
+        tasks.add("calc");
+        return  tasks;
+    }
+
+    /**
+     * Ответ на запрос сортировки по возрасту
+     * @return Список пользователей, возраст которых больше указанного в запросе
+     */
+    @GetMapping("/sort")//localhost:8080/tasks/sort
+    public List<User> sortUsersByAge()
+    {
+        return service.sortUsersByAge();
+    }
+
+    /**
+     * Ответ на запрос фильтрации по возрасту
+     * @param age Возраст, старше которого выводятся пользователи
+     * @return Список пользователей старше заданного возраста
+     */
+    @GetMapping("/filter/{age}")//localhost:8080/tasks/filter/23
+    public List<User> filterUsersByAge(@PathVariable("age") int age){
+        return service.filterUsersByAge(age);
+    }
+
+    /**
+     * Ответ на запрос вычисления среднего возраста
+     * @return Средний возраст пользователей
+     */
+    @GetMapping("/calc")//localhost:8080/tasks/calc
+    public double calculateAverageAge(){
+        return service.calculateAverageAge();
+    }
+}
